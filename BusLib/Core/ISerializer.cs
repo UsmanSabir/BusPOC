3095454483189b7479b8695db3075b99e29b0459
Usman <Usman@DESKTOP-RP9OELP>
2018-11-05 22:33:39 +0500

    State interfaces

---
+++ BusLib/Core/ISerializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusLib.Core
+{
+    public interface ISerializer
+    {
+        byte[] Serialize<T>(T item);
+
+        T Deserialize<T>(byte[] data);
+    }
+}
---
+++ BusLib/Core/IStateManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusLib.Core
+{
+    public interface IStateManager
+    {
+        IEnumerable<T> GetAllIncomplete<T>() where T : ICompletableState;
+
+        void Save<T>(T item) where T: ICompletableState;
+    }
+}
